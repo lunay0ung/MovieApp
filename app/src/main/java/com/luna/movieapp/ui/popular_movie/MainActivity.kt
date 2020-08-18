2d27b1c63f31e15fe40f190ed54f9c2d483abbbd
Soyoung Luna Yang <luna.ensocorro@gmail.com>
2020-08-18 17:42:57 +0900

    Finishing Up The App

--- app/src/main/java/com/luna/movieapp/ui/popular_movie/MainActivity.kt
+++ app/src/main/java/com/luna/movieapp/ui/popular_movie/MainActivity.kt
@@ -3,18 +3,68 @@ package com.luna.movieapp.ui.popular_movie
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.GridLayoutManager
 import com.luna.movieapp.R
+import com.luna.movieapp.data.api.TheMovieDBClient
+import com.luna.movieapp.data.api.TheMovieDBInterface
+import com.luna.movieapp.data.repository.NetworkState
 import com.luna.movieapp.ui.single_movie_details.SingleMovie
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel : MainActivityViewModel
+    lateinit var movieRepository : MoviePagedListRepository
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btn.setOnClickListener {
-            val intent = Intent(this, SingleMovie::class.java)
-            intent.putExtra("id",299534 )
-            this.startActivity(intent)
+        val apiService: TheMovieDBInterface = TheMovieDBClient.getClinet()
+
+        movieRepository = MoviePagedListRepository(apiService)
+
+        viewModel = getViewModel()
+
+        val movieAdapter = PopularMoviePagedListAdapter(this)
+        val gridLayoutManager = GridLayoutManager(this, 3)
+        gridLayoutManager.spanSizeLookup = object : GridLayoutManager.SpanSizeLookup() {
+            override fun getSpanSize(position: Int): Int {
+                val viewType = movieAdapter.getItemViewType(position)
+                if (viewType == movieAdapter.MOVIE_VIEW_TYPE) return  1    // 뷰타입이 MOVIE_VIEW_TYPE일 경우 3칸 중 1칸 차지  out of 3 span
+                else return 3                                              // 뷰타입이 NETWORK_VIEW_TYPE일 경우 모든 칸 차지
+            }
         }
+
+        rv_movie_list.layoutManager = gridLayoutManager
+        rv_movie_list.setHasFixedSize(true)
+        rv_movie_list.adapter = movieAdapter
+
+        viewModel.moviePagedList.observe(this, Observer {
+            movieAdapter.submitList(it) //pagedList에 변화가 있을 경우 어댑터로 전달
+        })
+
+        viewModel.networkState.observe(this, Observer {
+            progress_bar_popular.visibility = if (viewModel.listIsEmpty() && it == NetworkState.LOADING) View.VISIBLE else View.GONE
+            txt_error_popular.visibility = if (viewModel.listIsEmpty() && it == NetworkState.ERROR) View.VISIBLE else View.GONE
+
+            if (!viewModel.listIsEmpty()) {
+                movieAdapter.setNetworkState(it)
+            }
+        })
+    }
+
+    private fun getViewModel() : MainActivityViewModel {
+        return ViewModelProviders.of(this, object: ViewModelProvider.Factory{
+            override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+                @Suppress("UNCHECKED_CAST")
+                return MainActivityViewModel(movieRepository) as T
+            }
+        })[MainActivityViewModel::class.java]
     }
 }
