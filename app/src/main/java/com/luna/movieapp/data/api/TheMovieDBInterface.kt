39e8771d36342526cd29f15ec9d0b787558f272e
Soyoung Luna Yang <luna.ensocorro@gmail.com>
2020-08-18 16:39:57 +0900

    Creating The UI For Popular Movie

--- app/src/main/java/com/luna/movieapp/data/api/TheMovieDBInterface.kt
+++ app/src/main/java/com/luna/movieapp/data/api/TheMovieDBInterface.kt
@@ -1,9 +1,11 @@
 package com.luna.movieapp.data.api
 
 import com.luna.movieapp.data.vo.MovieDetails
+import com.luna.movieapp.data.vo.MovieResponse
 import io.reactivex.Single
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface TheMovieDBInterface {
 
@@ -12,6 +14,10 @@ interface TheMovieDBInterface {
     // https://api.themoviedb.org/3/movie/popular?api_key=78999dfcc0ed1877a71f77fc104a33b2&page=1
     // https://api.themoviedb.org/3/movie/299534?api_key=78999dfcc0ed1877a71f77fc104a33b2
     // https://api.themoviedb.org/3/
+
+
+    @GET("movie/popular")
+    fun getPopularMovie(@Query("page") page : Int) : Single<MovieResponse>
 
     @GET("movie/{movie_id}")
     fun getMovieDetails(@Path("movie_id") id: Int): Single<MovieDetails>
