27eedd2efa7c7ae9098a17359b7ad7d1fcc4ee48
Soyoung Luna Yang <luna.ensocorro@gmail.com>
2020-08-19 19:38:31 +0900

    object class와 companion object

--- app/src/main/java/com/luna/movieapp/data/api/TheMovieDBClient.kt
+++ app/src/main/java/com/luna/movieapp/data/api/TheMovieDBClient.kt
@@ -20,6 +20,25 @@ const val POSTER_PER_PAGE = 20
 // https://image.tmdb.org/t/p/w342/or06FN3Dka5tukK1e9sl16pB3iy.jpg
 
 
+/*
+object 클래스란
+
+인스턴스를 하나만 가진 싱글턴 클래스가 필요하다면 object 키워드를 붙이면 일반적인 방법으로 클래스를 선언하듯이 목표를 이룰 수 있다.
+해당 클래스는 오로지 하나의 인스턴스만 보유할 것이며, 해당 인스턴스의 이름은 클래스의 그것과 같다.
+-If you need a singleton - a class that only has got one instance -
+you can declare the class in the usual way, but use the object keyword instead of class:
+There will only ever be one instance of this class,
+and the instance (which is created the first time it is accessed, in a thread-safe manner) has got the same name as the class:
+
+-object declaration은 object 키워드를 통해서만 가능하다. 이렇게 선언된 객체는 프로그램 전체에서 공유할 수 있는 하나뿐인 객체이다.
+-어떤 변수, 메소드 등이 static 키워드로 선언되면, 해당 멤버는 자신이 속한 클래스의 어떤 객체가 생성되기 전에도 접근가능하다.
+
+
+-object로 클래스를 정의하면, 싱클턴(Singleton) 패턴이 적용되어 객체가 한번만 생성되도록 합니다.
+자바에서는 싱글턴 패턴을 적용하기 위해 꽤 많은 코드를 작성해야 했는데요, 코틀린에서는 object를 사용하면 이런 형식적인 코드(boilerplate)를 작성하지 않아도 됩니다.
+싱글턴으로 사용하는 방법 외에도, object는 익명객체를 생성할 때도 사용됩니다.
+https://codechacha.com/ko/kotlin-object-vs-class/
+ */
 object TheMovieDBClient {
 
     fun getClinet() : TheMovieDBInterface {
--- app/src/main/java/com/luna/movieapp/data/repository/NetworkState.kt
+++ app/src/main/java/com/luna/movieapp/data/repository/NetworkState.kt
@@ -8,6 +8,18 @@ enum class Status {
 
 class NetworkState (val status: Status, val msg : String) {
 
+    /*
+    **companion object**
+    클래스의 인스턴스보다는 클래스에 속한 함수나 프로퍼티가 필요할 경우 companion object 내에서 선언하면 된다.
+    companion object는 싱글턴이며, companion object의 멤버들은 멤버를 포함하는 클래스의 이름으로 바로 접근 가능하다.
+    (companion object에의 접근을 보다 명확하게 하고싶으면 이름을 붙일 수도 있다.)
+    If you need a function or a property to be tied to a class rather than to instances of it (similar to @staticmethod in Python),
+    you can declare it inside a companion object
+    The companion object is a singleton, and its members can be accessed directly via the name of the containing class
+    (although you can also insert the name of the companion object if you want to be explicit about accessing the companion object)
+
+    https://kotlinlang.org/docs/tutorials/kotlin-for-py/objects-and-companion-objects.html
+     */
     companion object {
         val LOADED : NetworkState //= NetworkState(Status.SUCCESS, "Success")
         val LOADING : NetworkState
